98686b5c848f478453189822ba699e54fbb6540d
Khamzeto <147990355+Khamzeto@users.noreply.github.com>
2024-12-09 22:42:38 +0300

    Update Carousel.tsx

--- next-app-template/components/ui/Carousel/Carousel.tsx
+++ next-app-template/components/ui/Carousel/Carousel.tsx
@@ -8,27 +8,12 @@ export function ImageCarousel() {
     <Carousel mt="10" withIndicators slideSize="100%" loop>
       <Carousel.Slide>
         <Image
-          src="https://cdn.esh-derevenskoe.ru/image/cache/catalog/c30f08-1150x450.jpg?v=3"
+          src="./2beram.jpeg"
           alt="Slide 1"
           height={600}
         />
       </Carousel.Slide>
-      <Carousel.Slide>
-        <Image
-          src="https://cdn.esh-derevenskoe.ru/image/cache/catalog/c30f08-1150x450.jpg?v=3"
-          alt="Slide 2"
-          fit="cover"
-          height={600}
-        />
-      </Carousel.Slide>
-      <Carousel.Slide>
-        <Image
-          src="https://cdn.esh-derevenskoe.ru/image/cache/catalog/c30f08-1150x450.jpg?v=3"
-          alt="Slide 3"
-          fit="cover"
-          height={600}
-        />
-      </Carousel.Slide>
+  
     </Carousel>
   );
 }
